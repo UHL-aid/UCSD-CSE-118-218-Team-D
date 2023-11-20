1bcc22b8a12ed6ebca6452f3d767d0bc6416001d
ArturoAmaya <a1amaya@ucsd.edu>
2023-11-19 16:33:58 -0800

    disconnect typo

--- tcp_server/app/main.py
+++ tcp_server/app/main.py
@@ -55,5 +55,5 @@ async def websocket_endpoint(websocket: WebSocket):
             #await websocket.send_text(f"Message text was: {data}")
             fake_data = generate_data()
             await websocket.send_text(f"Fake data was: {fake_data}")
-    except WebSocketDicsonnect:
+    except WebSocketDisconnect:
         print("client gone")
