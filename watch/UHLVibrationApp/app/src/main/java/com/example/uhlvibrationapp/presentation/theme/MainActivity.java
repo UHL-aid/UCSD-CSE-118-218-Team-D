d2497194597f5839164ee5d2abac90a2c0871eea
Julian Rahimi <78955065+ju-ra-96@users.noreply.github.com>
2023-12-07 13:02:03 -0800

    vibration integrated

--- watch/UHLVibrationApp/app/src/main/java/com/example/uhlvibrationapp/presentation/theme/MainActivity.java
+++ watch/UHLVibrationApp/app/src/main/java/com/example/uhlvibrationapp/presentation/theme/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.uhlvibrationapp.presentation.theme;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.TextView;
 
 import com.example.uhlvibrationapp.R;
@@ -19,104 +23,113 @@ public class MainActivity extends Activity {
     TextView receivedData;
     int counter;
     private WebSocket webSocket;
+    private String watchPosition; // Variable to store which wrist the watch is on
+    private Vibrator vibrator; // Vibrator instance
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        counter = 0;
         setContentView(R.layout.activity_main);
-        receivedData = (TextView) findViewById(R.id.textView2);
+        counter = 0;
+        receivedData = findViewById(R.id.textView2);
         receivedData.setText("haven't received anything yet");
-        connectWebSocket();
 
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE); // Get the Vibrator service
+        promptUserForWatchPosition();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    private void promptUserForWatchPosition() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Which wrist is the watch on?");
+        String[] options = {"Left", "Right"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                watchPosition = options[which];
+                connectWebSocket(); // Proceed with WebSocket connection after getting user input
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        // Close the WebSocket connection when the activity is destroyed
-        if (webSocket != null) {
-            webSocket.close(1000, "Activity destroyed");
-        }
-
-    }
-
-    public void connectWebSocket(){
+    private void connectWebSocket() {
         OkHttpClient client = new OkHttpClient();
         String address = "wss://b2a8-69-196-40-116.ngrok-free.app/ws";
-        //"wss://free.blr2.piesocket.com/v3/1?api_key=xgwn1SOmynhDhIEfci5yDZw9Iyg8eEAP4fK5SuMy&notify_self=1"
         Request request = new Request.Builder()
                 .url(address)
                 .build();
-        webSocket = client.newWebSocket(request, new WebSocketListener(){ //this is bad form... refactor later.
+        webSocket = client.newWebSocket(request, new WebSocketListener() {
             @Override
             public void onOpen(WebSocket webSocket, Response response) {
-                // WebSocket connection opened
-                super.onOpen(webSocket, response);
                 System.out.println("WebSocket connection opened");
             }
 
             @Override
             public void onMessage(WebSocket webSocket, String text) {
-                // Received a text message
-                super.onMessage(webSocket, text);
                 update(text);
                 System.out.println("Received message: " + text);
             }
 
             @Override
             public void onMessage(WebSocket webSocket, ByteString bytes) {
-                // Received a binary message
                 System.out.println("Received binary message: " + bytes.hex());
             }
 
             @Override
             public void onClosing(WebSocket webSocket, int code, String reason) {
-                // WebSocket is closing
-
                 System.out.println("WebSocket closing: " + code + " " + reason);
             }
 
             @Override
             public void onClosed(WebSocket webSocket, int code, String reason) {
-                // WebSocket is closed
                 System.out.println("WebSocket closed: " + code + " " + reason);
             }
 
             @Override
             public void onFailure(WebSocket webSocket, Throwable t, Response response) {
-                // WebSocket connection failure
                 System.out.println("WebSocket failure: " + t.getMessage());
             }
 
-            public void update(String message){
-                //RUMBLE THE DAMN WATCH!
-                //Or for now, just
+            private void update(String message) {
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
-                        // Update your UI components here
                         receivedData.setText("Received: " + message);
+                        handleVibration(message);
                         webSocket.send("ok " + counter);
                         counter++;
-                        //Should buzz different each time it updates!!!
-                        //Though, maybe a buzz should be an interrupt of sorts. 
                     }
                 });
             }
+
+            private void handleVibration(String message) {
+                if ("LEFT".equals(message) && "Left".equals(watchPosition)) {
+                    vibrateWatch();
+                } else if ("RIGHT".equals(message) && "Right".equals(watchPosition)) {
+                    vibrateWatch();
+                } else if ("BACK".equals(message)) {
+                    vibrateWatch();
+                }
+            }
+
+            private void vibrateWatch() {
+                if (vibrator.hasVibrator()) {
+                    // Vibrate with a default vibration pattern
+                    vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                }
+            }
         });
 
+        // Send initial message to WebSocket
         webSocket.send("Watch connected");
-
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webSocket != null) {
+            webSocket.close(1000, "Activity destroyed");
+        }
+    }
 }
